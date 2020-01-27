6fe708d866f9dcc6d7620d42a89680d14e2370cd
Zachary Meyer <zach.w.meyer@gmail.com>
2020-01-26 23:38:31 -0800

    image// test

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ const Index = () => (
                 <Clock/>
 
                 <div className="navigation-area">
-                    <Link href="/about">
+                    <Link href="/gallery">
                         <a className='gallery-btn'>gallery</a>
                     </Link>
                 </div>
