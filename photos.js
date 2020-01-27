b861c6e5d0f7c1cec0f048aa8700339aca1a2e71
Zachary Meyer <zach.w.meyer@gmail.com>
2020-01-27 00:01:31 -0800

    image// test

--- photos.js
+++ photos.js
@@ -5,49 +5,78 @@ export const photos = [
         height: 3
     },
     {
-        src: "/images/allthatshabu.jpg",
+        src: "/images/christmas-2019.jpg",
+        width: 5,
+        height: 4
+    },
+    {
+        src: "/images/self-realization-fellowship.jpg",
         width: 4,
         height: 3
     },
     {
-        src: "/images/allthatshabu.jpg",
+        src: "/images/disneyland-01.jpg",
+        width: 5,
+        height: 4
+    },
+    {
+        src: "/images/disneyland-02.jpg",
+        width: 5,
+        height: 3
+    },
+    {
+        src: "/images/bones-02-27-2019.jpg",
         width: 4,
         height: 3
     },
     {
-        src: "/images/allthatshabu.jpg",
+        src: "/images/zach-bday-2018.jpg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/images/mitsuwa-02-10-2019.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        src: "/images/haha01.jpg",
+        width: 3,
+        height: 3
+    },
+    {
+        src: "/images/haha02.jpg",
         width: 4,
         height: 3
     },
     {
-        src: "/images/allthatshabu.jpg",
+        src: "/images/haha03.jpg",
         width: 4,
         height: 3
     },
     {
-        src: "/images/allthatshabu.jpg",
+        src: "/images/haha04.jpg",
         width: 4,
         height: 3
     },
     {
-        src: "/images/allthatshabu.jpg",
+        src: "/images/haha05.jpg",
         width: 4,
         height: 3
     },
     {
-        src: "/images/allthatshabu.jpg",
+        src: "/images/haha06.jpg",
         width: 4,
         height: 3
     },
     {
-        src: "/images/allthatshabu.jpg",
+        src: "/images/n,n-12-06-2018.jpg",
         width: 4,
-        height: 3
+        height: 4
     },
     {
-        src: "/images/allthatshabu.jpg",
+        src: "/images/the-grove-12-06-2018.jpg",
         width: 4,
-        height: 3
+        height: 4
     },
-
 ];
