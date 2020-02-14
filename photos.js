7685662d5eae3835cf6317af43bd522cbe37ff8d
Zachary Meyer <zach.w.meyer@gmail.com>
2020-02-14 00:30:29 -0800

    added more photos

--- photos.js
+++ photos.js
@@ -1,5 +1,45 @@
 export const photos = [
     {
+        src: "/static/images/image0.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image1.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image2.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image3.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image4.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image5.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image6.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image7.jpeg",
+        width: 3,
+        height: 4
+    },
+    {
         src: "/static/images/allthatshabu.jpg",
         width: 4,
         height: 3
@@ -79,4 +119,295 @@ export const photos = [
         width: 4,
         height: 4
     },
+
+    {
+        src: "/static/images/image8.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image9.jpeg",
+        width: 3,
+        height: 4
+    },
+    {
+        src: "/static/images/image10.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image11.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image12.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image13.jpeg",
+        width: 3,
+        height: 4
+    },
+    {
+        src: "/static/images/image14.jpeg",
+        width: 3,
+        height: 4
+    },
+    {
+        src: "/static/images/image15.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image16.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image17.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image18.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image19.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image20.jpeg",
+        width: 3,
+        height: 4
+    },
+    {
+        src: "/static/images/image21.jpeg",
+        width: 3,
+        height: 4
+    },
+    {
+        src: "/static/images/image22.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image23.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image24.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image25.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image26.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image27.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image28.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image29.jpeg",
+        width: 3,
+        height: 4
+    },
+    {
+        src: "/static/images/image30.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image31.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image32.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image33.jpeg",
+        width: 3,
+        height: 4
+    },
+    {
+        src: "/static/images/image34.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image35.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image36.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image37.jpeg",
+        width: 3,
+        height: 4
+    },
+    {
+        src: "/static/images/image38.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image39.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image40.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image41.jpeg",
+        width: 3,
+        height: 4
+    },
+    {
+        src: "/static/images/image42.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image43.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image44.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image45.jpeg",
+        width: 3,
+        height: 4
+    },
+    {
+        src: "/static/images/image46.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image47.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image48.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image49.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image50.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image51.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image52.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image53.jpeg",
+        width: 3,
+        height: 4
+    },
+    {
+        src: "/static/images/image54.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image55.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image56.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image57.jpeg",
+        width: 3,
+        height: 4
+    },
+    {
+        src: "/static/images/image58.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image59.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image60.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image61.jpeg",
+        width: 3,
+        height: 4
+    },
+    {
+        src: "/static/images/image62.jpeg",
+        width: 4,
+        height: 4
+    },
+    {
+        src: "/static/images/image63.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image64.jpeg",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "/static/images/image7.png",
+        width: 3,
+        height: 4
+    }
 ];
